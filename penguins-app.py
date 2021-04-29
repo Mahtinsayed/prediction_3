979d1e67313f7df8249030ebd350f33498070367
Mahtinsayed <53224502+Mahtinsayed@users.noreply.github.com>
2021-04-29 17:58:43 +0600

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -9,7 +9,7 @@ st.write("""
 
 This app predicts the **Palmer Penguin** species!
 
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+Data obtained from the [palmerpenguins library](https://drive.google.com/file/d/13UAb77GVcWiCNYy17ZzQuaX7GVq9I1jy/view?usp=sharing) in R by Allison Horst.
 """)
 
 st.sidebar.header('User Input Features')
