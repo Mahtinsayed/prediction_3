3ad2ba1c6ae78bad6860471287099cf4afca0a4e
Mahtinsayed <53224502+Mahtinsayed@users.noreply.github.com>
2021-04-30 17:29:22 +0600

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -24,7 +24,7 @@ def load_data():
     url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQWPoM1zq7FrW06SYxMIWboclt5FQoRB_rgWFRjzhvY-YN9NlZbm_TaB4Je2e0KulhGX8BmD3inUhI1/pub?gid=775382138&single=true&output=csv'
     html = pd.read_html(url, header = 0)
     df = html[0]
-    return df
+ 
 
 
 sector = df.groupby('penguins_cleaned.csv')
